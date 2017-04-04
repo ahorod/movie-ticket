13e3e507a42cd6130bbe017049cb88bffd72aa07
Anna Horodetska and Ilene Gorski <anna@>
2017-04-04 12:08:16 -0700

    add beginnign price calculation conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,92 +1,53 @@
 //business logic
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
-  this.addresses = [];
-}
-
-function Address(street, city, state, addressType) {
-  this.street = street;
-  this.city = city;
-  this.state = state;
-  this.addressType = addressType;
-}
-
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-}
-
-Address.prototype.fullAddress = function() {
-  console.log(this.street, this.city, this.state, this.addressType)
-  return this.addressType + ": " + this.street + ", " + this.city + ", " + this.state;
-}
-
+//constructor for Ticket
+function Ticket(movie, time, age) {
+  this.movie = movie;
+  this.time = time;
+  this.age = parseInt(age);
+  this.price = 20;
+}//close constructor
+//prototype generates ticket information
+Ticket.prototype.ticketInfo = function() {
+  return this.movie + " " + this.time + " " + this.age ;
+}//close prototype
+//prototype calculates ticket price
+Ticket.prototype.ticketPrice = function() {
+  console.log(this.age);
+  if (this.age < 12) {
+    this.price -=5;
+    console.log(this.price);
+  }
+  return(this.price);
+}//close prototype
+//function resets form
 function resetFields() {
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
-    $("select.new-type").val("Home");
-    $(".removable").remove();
-
+    $("select.movie").val("");
+    $("input#time").val("");
+    $("input#input-age").val("");
 }
 
 // user interface logic
 $(document).ready(function() {
 
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address removable">' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-state">State</label>' +
-                                   '<input type="text" class="form-control new-state">' +
-                                 '</div>' +
-                               '</div>' +
-                               '<div>' + '<label for="addressType">Type of Address</label><br>' +
-                               '<select class="form-group" class="new-type">' + '<option>Home</option>' +
-                               '<option>Work</option>' + '<option>Mailing</option>' + '<option>Other</option>' +
-                               '</select>' + '</div>'
-                             );
-  });
 
-  $("form#new-contact").submit(function(event) {
+  $("form#new-ticket").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var inputtedMovie = $("select.movie").val();
+    var inputtedTime= $("input#time").val();
+    var inputtedAge= $("input#input-age").val();
 
-    $(".new-address").each(function() {
-      var inputtedType = $(this).find("select.new-type").val();
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
+    var newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
 
-      console.log(inputtedType, inputtedStreet,inputtedCity, inputtedState);
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedType);
-      newContact.addresses.push(newAddress);
-    });
+    var resultPrice = newTicket.ticketPrice();
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-    });
+      $("#show-price").show();
+      $("#new-movie").text(newTicket.movie);
+      $("#new-time").text(newTicket.time);
+      $("#new-age").text(newTicket.age);
+      $("#new-price").text(resultPrice);
+      console.log(inputtedMovie, inputtedTime, inputtedAge);
 
     resetFields();
 
